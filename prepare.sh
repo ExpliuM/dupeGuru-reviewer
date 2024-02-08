00d148ab715f8802123d1f9fc7c44d2c75234c00
Alexander Khvolis <alexander.khvolis@keysight.com>
2024-02-08 23:27:13 +0100

    prepare and run scripts updated

--- prepare.sh
+++ prepare.sh
@@ -13,6 +13,8 @@ elif [[ "$OSTYPE" == *"darwin"* ]]; then
   # App dependencies
   brew install ffmpeg
   brew install libmagic
+  brew install python@3.11
+  brew install qt6
   
   # Build tools
   brew install create-dmg
@@ -23,13 +25,7 @@ else
 fi
 
 # init dupeGuru-result-reviewer env.
-python3 -m venv ./env
-./env/bin/python3 -m pip install --upgrade pip
-./env/bin/pip3 install --upgrade pip
-
-# dupeGuru-result-reviewer install requirements
-./env/bin/pip3 install -r requirements.txt
-# ./env/bin/pip3 install --global-option=build_ext --global-option="-I$(brew --prefix libheif)/include/" --global-option="-L$(brew --prefix libheif)/lib/" git+https://github.com/carsales/pyheif.git
-# ./env/bin/pip3 install git+https://github.com/carsales/pyheif.git
-
-source ./env/bin/activate
+rm -rf .venv
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
--- run.sh
+++ run.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 
-# if [[ "$OSTYPE" == *"Ubuntu"* ]]; then
-#   echo Preparing env for Ubuntu OS
-
-# elif [[ "$OSTYPE" == *"darwin"* ]]; then
-#   echo Preparing env for MacOS
-#   export PATH="$PATH:/opt/homebrew/Cellar/qt5/bin/"
-#   export PATH="$PATH:/usr/local/opt/sphinx-doc/bin"
-# else
-#   echo "Currently only Ubuntu and OSX"
-#   exit
-# fi
-
-./env/bin/python3 dupeGuru-reviewer.py
+source .venv/bin/activate
+python3 dupeGuru-reviewer.py
